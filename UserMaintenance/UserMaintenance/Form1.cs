a2d984ad16dc505f306aa0c3134998e2973cc0c9
Zsombor Szatmári <zsombor.szatmari@stud.uni-corvinus.hu>
2021-10-01 23:15:39 +0200

    Mentés

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace UserMaintenance
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
             listUsers.DisplayMember = "FullName";
+            button1.Text = Resource1.Save;
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -32,6 +34,22 @@ namespace UserMaintenance
                 u.FullName = txtLastname.Text;
             };
             users.Add(u);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using(StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var s in users)
+                {
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
