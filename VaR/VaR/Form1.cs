67302c6ddf3f5c9f3ef5af754731600e6c63fdb0
Zsombor Szatmári <zsombor.szatmari@stud.uni-corvinus.hu>
2021-10-22 10:58:05 +0200

    Add portfolio value

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -21,7 +21,7 @@ namespace VaR
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
-            CreatePortfolio()
+            CreatePortfolio();
         }
 
         private void CreatePortfolio()
@@ -31,6 +31,20 @@ namespace VaR
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dataGridView2.DataSource = Portfolio;
+        }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
