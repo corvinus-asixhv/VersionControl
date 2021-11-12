8759c5f35c256c31750536d407b4905dfeb3453d
Zsombor Szatmári <zsombor.szatmari@stud.uni-corvinus.hu>
2021-11-12 22:47:43 +0100

    Futószalag létrehozása

--- week08/week08/Form1.cs
+++ week08/week08/Form1.cs
@@ -11,9 +11,10 @@ using week08.Entities;
 
 namespace week08
 {
-    private List<Ball> _balls = new List<Ball>();
+   
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
         private BallFactory _factory;
         public BallFactory Factory
         {
@@ -24,6 +25,32 @@ namespace week08
         {
             InitializeComponent();
             Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
