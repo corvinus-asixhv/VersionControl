6a3bf6e73b94dba291476d5a35ac6d31f580de1e
Zsombor Szatmári <zsombor.szatmari@stud.uni-corvinus.hu>
2021-11-12 23:34:33 +0100

    Színválasztó gomb

--- week08/week08/Form1.Designer.cs
+++ week08/week08/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace week08
             this.buttonBall = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -84,11 +85,22 @@ namespace week08
             this.label1.TabIndex = 3;
             this.label1.Text = "Coming next";
             // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnColor.Location = new System.Drawing.Point(31, 12);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnColor);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.buttonBall);
@@ -108,6 +120,7 @@ namespace week08
         private System.Windows.Forms.Button buttonBall;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 
